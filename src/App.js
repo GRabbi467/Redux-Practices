12137633c6f209a260fbcf6c0de2dec6a73be1d5
Golam Rabbi <57221660+GRabbi467@users.noreply.github.com>
2024-01-16 03:42:39 +0600

    git initialized

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className='text-4xl text-violet-800'>Redux Practices in React Js</h1>
+    </div>
+  );
+}
+
+export default App;
