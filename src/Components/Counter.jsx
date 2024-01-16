ee5ab9f1f9f6456eb64368d451a7a4b48c9d0afc
Golam Rabbi <57221660+GRabbi467@users.noreply.github.com>
2024-01-16 20:36:44 +0600

    Lifting state up in react js

---
+++ src/Components/Counter.jsx
@@ -0,0 +1,30 @@
+const Counter = ({handleIncre,handleDec,id}) => {
+  
+    return (
+        <div>
+            <div className="max-w-md mx-auto mt-10 space-y-5">
+                <div
+                    className="p-4 h-auto flex flex-col items-center justify-center space-y-5 bg-white rounded shadow"
+                >
+                    <div className="text-2xl font-semibold"></div>
+                    <div className="flex space-x-3">
+                        <button
+                            className="bg-indigo-400 text-white px-3 py-2 rounded shadow"
+                            onClick={()=>handleIncre(id)}
+                        >
+                            Increment
+                        </button>
+                        <button
+                            className="bg-red-400 text-white px-3 py-2 rounded shadow"
+                             onClick={()=>handleDec(id)}
+                        >
+                            Decrement
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Counter;
---
+++ src/Components/Stats.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Stats = ({count}) => {
+    return (
+       <div className='max-w-md mx-auto mt-10 space-y-5'>
+         <div className="p-4 h-40 mt-5 flex flex-col items-center justify-center space-y-5 bg-white rounded shadow">
+            <div className="text-2xl font-semibold">Total count: {count}</div>
+        </div>
+       </div>
+        
+    );
+};
+
+export default Stats;
